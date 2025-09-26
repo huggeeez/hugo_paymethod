c8f2fee849b835eaf01f9abd46c72a31a26b2c13
Huggeeez <122524598+huggeeez@users.noreply.github.com>
2025-09-26 23:05:27 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Hugo'
+description 'Payment method script with Cash/Card options.'
+version '1.0.0'
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/main.js'
+}
